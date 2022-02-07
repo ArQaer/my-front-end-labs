f5eac8a0e8f61564afd320598eb00148e27aad89
Yaroslav Volkov <arqaer@gmail.com>
2022-02-07 19:02:05 +0200

    udate

--- lab 5/lab-work-5.js
+++ lab 5/lab-work-5.js
@@ -1,5 +1,5 @@
 const listOfCars = [];
-const needToEnterCars = 5;
+const needToEnterCars = 3;
 let carLeftToEnter = needToEnterCars + 1;
 const engineDisplacementLimit = 2.0; 
 let carsWithBigEngine = 0;
@@ -11,19 +11,19 @@ for (let i = 0; i < needToEnterCars; i++) {
         ModelName : '',
         EngineDisplacement : 0
     }
-    listOfCars[i].FirmName = String(prompt(`Car left: ${carLeftToEnter} \nEnter brand of car:`));
-    if (listOfCars[i].FirmName === '' || listOfCars[i].FirmName === 'null'){
+    listOfCars[i].FirmName = prompt(`Car left: ${carLeftToEnter} \nEnter brand of car:`);
+    if (listOfCars[i].FirmName === '' || listOfCars[i].FirmName === null){
         do {
             alert("You are not enter any name")
-            listOfCars[i].FirmName = String(prompt(`Car left: ${carLeftToEnter} \nEnter brand of car:`));
-        } while (listOfCars[i].FirmName === '' || listOfCars[i].FirmName === 'null')
+            listOfCars[i].FirmName = prompt(`Car left: ${carLeftToEnter} \nEnter brand of car:`);
+        } while (listOfCars[i].FirmName === '' || listOfCars[i].FirmName === null)
     }
-    listOfCars[i].ModelName = String(prompt(`Car left: ${carLeftToEnter} \nEnter model of car:`));
-    if (listOfCars[i].ModelName ==='' || listOfCars[i].ModelName === 'null'){
+    listOfCars[i].ModelName = prompt(`Car left: ${carLeftToEnter} \nEnter model of car:`);
+    if (listOfCars[i].ModelName ==='' || listOfCars[i].ModelName === null){
         do {
             alert("You are not enter any model")
-            listOfCars[i].ModelName = String(prompt(`Car left: ${carLeftToEnter} \nEnter model of car:`));
-        } while (listOfCars[i].ModelName === '' || listOfCars[i].ModelName === 'null')
+            listOfCars[i].ModelName = prompt(`Car left: ${carLeftToEnter} \nEnter model of car:`);
+        } while (listOfCars[i].ModelName === '' || listOfCars[i].ModelName === null)
     }
     listOfCars[i].EngineDisplacement = Number(prompt(`Car left: ${carLeftToEnter} \nEnter engine displacement of car(example:1.6 or 3.5 or 2.0):`));
     if (isNaN(listOfCars[i].EngineDisplacement) || listOfCars[i].EngineDisplacement === 0){
@@ -37,8 +37,8 @@ for (let i = 0; i < needToEnterCars; i++) {
 console.log(listOfCars);
 
 for (let i = 0; i < listOfCars.length; i++) {
-    if (listOfCars[i].EngineDisplacement > engineDisplacementLimit) {
-        console.log(`${listOfCars[i].FirmName} ${listOfCars[i].ModelName} has engine ${listOfCars[i].EngineDisplacement} l. it's more than ${engineDisplacementLimit} l.`);
+    if (listOfCars[i].EngineDisplacement >= engineDisplacementLimit) {
+        console.log(`${listOfCars[i].FirmName} ${listOfCars[i].ModelName} has engine ${listOfCars[i].EngineDisplacement} l. it's more than or equal ${engineDisplacementLimit} l.`);
         carsWithBigEngine++;
     }
 }
